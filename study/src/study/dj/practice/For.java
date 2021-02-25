44802cfbe0e338c4dc3691d9b3379d77bdc1a350
seodj93 <67113707+seodj93@users.noreply.github.com>
2021-02-25 23:57:24 +0900

    02/25

---
+++ study/src/study/dj/practice/For.java
@@ -0,0 +1,51 @@
+package study.dj.practice;
+
+import java.util.Scanner;
+
+public class For {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		
+	}
+	
+//	A+B - 3	
+//	public static void main(String[] args) {
+//
+//		Scanner s = new Scanner(System.in);
+//		
+//		int t = s.nextInt();
+//		
+//		int arr[] = new int[t];
+//		
+//		for(int i=0; i<t; i++) {
+//			
+//			int a = s.nextInt();
+//			int b = s.nextInt();
+//			
+//			arr[i] = a+b;
+//		}
+//		
+//		for(int r: arr) {
+//			System.out.println(r);
+//		}
+//		
+//		
+//	}
+	
+	
+//	nÀ» ÀÔ·Â¹Þ¾Æ n°ö¼À ¸¸µé±â
+//	public static void main(String[] args) {
+//		
+//		Scanner s = new Scanner(System.in);
+//		
+//		int n = s.nextInt();
+//		
+//		for(int i=1; i<10; i++) {
+//			System.out.println(n + " * " + i + " = " +  n*i);
+//		}
+//	}
+
+}
--- study/src/study/dj/practice/Study_2021.java
+++ study/src/study/dj/practice/Study_2021.java
@@ -1,10 +1,59 @@
 package study.dj.practice;
 
+import java.util.Scanner;
+
 public class Study_2021 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("테스트");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+//		x,y축 그래프 (1,2,3,4) 분면 맞추기
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		
+//		if(a>0) {
+//			if(b>0) {
+//				System.out.println("1");
+//			}else {
+//				System.out.println("4");
+//			}
+//		}else {
+//			if(b>0) {
+//				System.out.println("2");
+//			}else {
+//				System.out.println("3");
+//			}
+//		}
+		
+//		윤년 알아내는 문제
+//		int year = sc.nextInt();
+//		
+//		if(year%4==0) {
+//			if(year%400==0) {
+//				System.out.println("1");
+//			}else if(year%100==0){
+//				System.out.println("0");
+//			}else {
+//				System.out.println("1");
+//			}
+//		}else {
+//			System.out.println("0");
+//		}
+		
+		
+//		a와b 수비교 하는 문제
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		
+//		if(a>b) {System.out.println(">");}
+//		if(a<b) {System.out.println("<");}
+//		if(a==b) {System.out.println("=");}
 	}
 
 }
