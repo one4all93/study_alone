aaa551f63770b15f62c6d62f466b89e264a6cf07
seodj93 <67113707+seodj93@users.noreply.github.com>
2021-02-26 03:45:54 +0900

    Update For.java

--- study/src/study/dj/practice/For.java
+++ study/src/study/dj/practice/For.java
@@ -1,13 +1,33 @@
 package study.dj.practice;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class For {
 
 	public static void main(String[] args) {
 
-		Scanner s = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//System.out.println(br);
+		try {
+			String input = br.readLine();
+			
+			int t = Integer.parseInt(input);
+			
+			for(int i = 0; i<t;i++) {
+				StringTokenizer st = new StringTokenizer(input," ");
+				System.out.println(st);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		
+		//BufferedWriter bw = new BufferedWriter(new InputStreamReader(System.in));
 		
 	}
 	
